56826a0b7496a2f45ed249cdd734897da8ea3469
codigoinerte <fmartinez.bpe@gmail.com>
2025-08-29 17:43:50 -0500

    fix: cambio de timeout

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def getPageData():
         response = requests.get(
             url,
             headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"},
-            timeout=3200
+            timeout=500
         )
         cities = []
         states = []
